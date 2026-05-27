3252c96a5369229c1d78a219647fb9227c48d608
mrahatcreative <mrahatcreative@gmail.com>
2026-05-27 13:36:21 +0600

    fix: bundle admin_console in backend repo; remove separate admin_console container; fix docker-compose paths

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/uploads', express.static(uploadDir));
-app.use('/admin', express.static(path.join(__dirname, '..', 'admin_console')));
+app.use('/admin', express.static(path.join(__dirname, 'admin_console')));
 
 // Log requests
 app.use((req, res, next) => {
